29a99c2a719e4a667fb91a15da35bab6f21b652c
Kerim <126254263+kerimhmad@users.noreply.github.com>
2023-08-17 15:21:54 +0200

    Add files via upload

---
+++ kerim_clientloader/fxmanifest.lua
@@ -0,0 +1,21 @@
+game "gta5"
+fx_version "cerulean"
+
+lua54 "yes"
+
+author "der69er (discord.gg/gQeKYvDTQt)"
+version "1.0.7"
+
+
+shared_scripts {
+    "resources.lua",
+    "shared/clientloader.lua"
+}
+
+server_scripts {
+    "server/main.lua"
+}
+
+escrow_ignore {
+    "resources.lua"
+}
