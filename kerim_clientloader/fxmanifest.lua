40d4e70765265b22838cefe391ebbee87c35dce9
Kerim <126254263+kerimhmad@users.noreply.github.com>
2023-10-13 00:58:39 +0200

    Update fxmanifest.lua

--- kerim_clientloader/fxmanifest.lua
+++ kerim_clientloader/fxmanifest.lua
@@ -8,15 +8,9 @@ version "1.0.8"
 
 
 shared_scripts {
-    "resources.lua",
-
     "shared/clientloader.lua"
 }
 
 server_scripts {
     "server/main.lua"
-}
-
-escrow_ignore {
-    "resources.lua"
 }
